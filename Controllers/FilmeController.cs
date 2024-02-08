d2e4a88ea3bf373da8abd6f8db6ce1be5f4588e4
Lucas.Pinheiro <lucas.pinheiro@autoglass.com.br>
2024-02-08 17:34:00 -0300

    update

--- Controllers/FilmeController.cs
+++ Controllers/FilmeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using FilmesAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 using static System.Net.WebRequestMethods;
 
@@ -14,8 +15,10 @@ public class FilmeController : ControllerBase
 {
     private static List<Filme> filmes = new List<Filme>();
 
-    public void AdicionarFilme(Filme filme)
+    [HttpPost]
+    public void AdicionarFilme([FromBody] Filme filme)
     {
         filmes.Add(filme);
+        Console.WriteLine("Codigo do filme: {0}, Titulo do filmes: {1}, Genero do filme: {2}, Duração do filme: {3}", filme.Id, filme.Titulo, filme.Genero, filme.Duracao);
     }
 }
--- obj/Debug/net6.0/FilmesAPI.AssemblyInfo.cs
+++ obj/Debug/net6.0/FilmesAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FilmesAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e2c3269a46b7fdccebdc8740db7718c9ed8ca33")]
 [assembly: System.Reflection.AssemblyProductAttribute("FilmesAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FilmesAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
