2ecb585d31c1997d42c9778638321bd6b784e431
Lucas.Pinheiro <lucas.pinheiro@autoglass.com.br>
2024-02-26 13:01:24 -0300

    update

--- Data/FilmeContext.cs
+++ Data/FilmeContext.cs
@@ -5,7 +5,9 @@ namespace FilmesAPI.Data;
 
 public class FilmeContext : DbContext
 {
+#pragma warning disable CS8618 // O campo não anulável precisa conter um valor não nulo ao sair do construtor. Considere declará-lo como anulável.
     public FilmeContext(DbContextOptions<FilmeContext> opts)
+#pragma warning restore CS8618 // O campo não anulável precisa conter um valor não nulo ao sair do construtor. Considere declará-lo como anulável.
         : base(opts)
     {
 
